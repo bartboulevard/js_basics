aeb98592555c075c1dada8237dccc792b81dad07
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2022-09-20 11:18:09 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!")
+// variables
+// let
+let name = 'Kaspar Pedaja'
+name = 'new name'
+
+// const
+const surname = 'Pedaja'
+// surname = 'dasda' - error
+
+// arrays
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+// objects
+const person = {
+    firstname: 'Kaspar',
+    surname: 'Pedaja'
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = 'IKT'
+console.log(person)

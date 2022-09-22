795d4592acf36b88d48a2b86a1ab4b0e01e206ac
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2022-09-22 13:05:57 +0300

    Objects and attributes

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const numbers1  = [46, 79, 61, 102, 84, 33]
-const numbers2 = new Array(43, 465, 421, 33, 4)
+const person = {
+    firstname: 'Juhan',
+    surname: 'Parts',
+    age: 56,
+    email: 'juhan.parts@gmail.com',
+    hobbies: ['jalgpall', 'korvpall'],
+    address: {
+        city: 'Võru',
+        county: 'Võrumaa'
+    },
+    getBirthYear: function (){
+        return 2022 - this.age
+    }
+}
 
 let val
+val = person
+val = person.firstname
+val = person["surname"]
+val = person["hobbies"][1]
+val = person.hobbies[0]
+val = person.address.city
+val = person.address["city"]
+val = person.getBirthYear()
 
-val = numbers1.length
-val = numbers2.length
+console.log(val)
 
-val = numbers1[3]
-val = numbers1.indexOf(61)
-val = Array.isArray(null)
+for(let i = 0; i < person.hobbies.length; i++){
+    console.log(person.hobbies[i])
+}
 
-
-numbers1.push(88)
-numbers1.pop()
-numbers1.unshift(6553)
-numbers1.shift()
-
-
-val = numbers1.concat(numbers2)
-
-console.log(numbers1)
-val = numbers1.sort(function (x, y) {
-    return y - x
+person.hobbies.forEach((hobby, index) => {
+    console.log(hobby + ' on element indeksiga ' + index)
 })
-
-
-console.log(numbers1)
